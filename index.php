0f7a7445c55c0f07bd997f0817797508af6dedfb
lucaluke88 <lucacostantino88@gmail.com>
2015-03-11 10:41:20 +0100

    primo commit

---
+++ index.php
@@ -0,0 +1,14 @@
+
+
+<html>
+<head>
+<title> Pathway</title>
+<body>
+<h2> Progetto Bioinformatica </h2>
+<div> 
+	<form action="metodo.php" method="POST">
+		<input type="text" length="100" name="query" value="inserisci query qui"/>
+		<input type="submit"/> <input type="reset"/>
+	</form>
+</div>
+</body>
