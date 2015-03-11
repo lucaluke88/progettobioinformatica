4aabf4774fe41c53d3573c0238a60d4f9be3c233
lucaluke88 <lucacostantino88@gmail.com>
2015-03-11 18:54:38 +0100

    Levato il search.php per adesso, il form redirecta alla pagina stesso (che ha senso, dato che l'output del form va mostrato nella pagina stessa).

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 	<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
 	"http://www.w3.org/TR/html4/loose.dtd">
 	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 		<title>DB Unificato per Pathways - UNICT (Costantino-Ramo)</title>
 		<link rel="stylesheet" href="stile.css"/>
 	</head>
@@ -9,14 +10,20 @@
 			<div id="container">
 				<h1> Unified Database for Pathway Analysis </h1>
 				<div id="searchform"> 
-					<form action="search.php" method="POST">
-						<input id="searchbox" type="text" name="query" value="insert your query here"/>
+					<form action="#" method="POST">
+						<input id="searchbox" type="text" name="query"/>
 						</br></br>
 						<input type="submit"/> <input type="reset"/>
 					</form>
 				</div>
 				<div id="output">
-					
+					<?php
+						include('PhpOrient.php');
+						if(isset($_POST['query'])) {
+							$query = $_POST['query'];
+							echo "La tua query è : ".$query;
+						}
+					?>
 				</div>
 				<div id="footer">
 					Developed by: Illuminato Luca Costantino & Daniela Ramo
