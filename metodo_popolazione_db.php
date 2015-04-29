9d575773bc4c71588e538b0852f850b26f1b2b99
Luca Luke Costantino <lucacostantino88@gmail.com>
2015-04-29 15:57:55 +0200

    mao

--- metodo_popolazione_db.php
+++ metodo_popolazione_db.php
@@ -14,6 +14,7 @@ ini_set('display_errors', '1');
  * 	INIZIALIZZAZIONE REPOSITORY ======================================================================
  */ 
 
+
 //Repository delle entry         
 $entryRepo = new \Mithril\Pathway\Repository\Entry\Entry();
 
@@ -39,11 +40,10 @@ $entryTypeRepo->add($geneEntryType);
 $entryTypeRepo->add($orthologEntryType);
 //costruisco i tipi di relazione maplink, ecrel 
 $maplinkRelationType = new \Mithril\Pathway\Relation\Type(['name' => 'maplink']);
-$ecrellinkRelationType = new \Mithril\Pathway\Relation\Type(['name' => 'ecrel']);
+$ecrellinkRelationType = new \Mithril\Pathway\Relation\Type(['name' => 'ECrel']);
 //e li aggiungo alla relativa repository
 $relationTypeRepo->add($maplinkRelationType);
 $relationTypeRepo->add($ecrellinkRelationType);
-
 
 /*
  * OPERAZIONI DI LETTURA =============================================================================
@@ -51,17 +51,15 @@ $relationTypeRepo->add($ecrellinkRelationType);
 
 // elenco di tutte le pathway umane
 // http://rest.kegg.jp/list/pathway/hsa
-$pathwaylist = file_get_contents('http://rest.kegg.jp/list/pathway/hsa');
-while(!feof($pathwaylist)){
-    $p = fgets($pathwaylist);
-    $p_code = substr($p, 5, 12);
-	echo $p_code;
-	$url = ("http://www.kegg.jp/kegg-bin/download?entry=".$p_code[1]."&format=kgml");
-	echo $url;
+$pathwaylist = explode("\n", file_get_contents('http://rest.kegg.jp/list/pathway/hsa'));
+foreach ($pathwaylist as $p) {
+	echo "pathway n".$cont;
+	echo "</br>";
+	$url = ("http://www.kegg.jp/kegg-bin/download?entry=".substr($p, 5, 8)."&format=kgml");
 	// leggo dall'url il mio xml, lo analizzo e per ogni elemento, creo l'entry o la relation più opportuna
-	//readElementsFromXML($url);
+	readElementsFromXML($url);
 }
-fclose($file);
+
  
 /*
  *	MIE FUNZIONI ================================================================================================
@@ -80,55 +78,75 @@ function createRelation($entry1, $entry2, $type, $subType, $pathway)
     ]);
 }
 
-// leggiamo la stringa xml e analizziamo i vari elementi: se è ENTRY, creeremo un gene, altrimenti una relazione
 function readElementsFromXML($url)
 {
-	// $url contiene un url ad una specifica pathway (in formato xml)
 	$xmlstring = file_get_contents($url); // il contenuto della pagina inserito in questa variabile
-	$xmlobj = simplexml_load_string($xmlstring); // oggetto xml di tutta la pathway
-	if ($xmlobj -> getName() == "pathway")
+	echo $xmlstring;
+	$xml_pathway_obj = simplexml_load_string($xmlstring); // oggetto xml di tutta la pathway
+	
+	
+	/* NOMI VARIABILI
+	 * $xml_pathway_obj = è il nodo root dell'xml, contiene le info sulla pathway
+	 * $xml_item = identifica tutti i nodi di primo livello (cioè entries
+	 * e relations)
+	 */
+	
+	
+	if ($xml_pathway_obj -> getName() == "pathway")
 	{
 		// root: informazioni e tag della pathway
-		$mypathway = createPathway($xmlobj['name'], $xmlobj['org'], $xmlobj['title'], $xmlobj['image'], $xmlobj['link']);
-		//e la aggiungo alla repository delle pathway
-        $pathwayRepo->add($mypathway); // con questo comando aggiungiamo la pathway al repo
-        // -------------------------------------------------------------------------------
-        // guardiamo tutti i figli (che sono gene/orthology o relation)    
-        foreach ($xmlobj->children() as $xmlobj_child)
+		
+		$mypathway = createPathway($xml_pathway_obj['name'], $xml_pathway_obj['org'], $xml_pathway_obj['title'], $xml_pathway_obj['image'], $xml_pathway_obj['link']);
+		$pathwayRepo->add($mypathway); // con questo comando aggiungiamo la pathway al repo
+        
+        // guardiamo tutti i figli (che sono gene/orthology o relation) 
+        foreach ($xml_pathway_obj->children() as $xml_item)
 		{
-			if ($xmlobj_child -> getName() == "entry")
+			if ($xml_item -> getName() == "entry")
 			{
-				$graph_entry = $xmlobj_child -> children(); // figlio "graphic"
-				// controllare bene $xmlobj_child['name'],$xmlobj_child['name']
-				// il tag name contiene gli alias, il nome da memorizzare va preso da
-				// hsa:UN_NUMERO il numero contenuto va posto come id nell'oggetto entry
-				$entry = createEntry($xmlobj['name'],$xmlobj_child['name'],$xmlobj_child['name'],$xmlobj_child['type'],$xmlobj_child['link'],$graph_entry);
-	            //e la aggiungo alla repository delle entry
-	            $entryRepo->add($entry);
+				// uso il foreach perchè altrimenti non mi legge il figlio ma fa una sola iterazione
+				foreach ($xml_item->children() as $graph_child) 
+				{
+					/* 	Il tag name contiene gli alias, il nome da memorizzare va preso da
+				 		hsa:UN_NUMERO il numero contenuto va posto come id nell'oggetto entry.
+				 		Ogni alias è nel formato hsa:ID, dobbiamo creare 3 entry o fare una lista di 3 id?
+					*/
+				
+					// Qui mi ricavo gli N id (gli alias) e creo N entry nel db
+					$aliases_list = explode(" ", $xml_item['name']);
+					foreach ($aliases_list as $alias) 
+					{
+						$entry_id = substr($alias, 4);
+						$entry = createEntry($xml_pathway_obj['name'],$entry_id,$xml_item['name'],$xml_item['type'],$xml_item['link'],$graph_child);
+		            	$entryRepo->add($entry);
+					}
+					
+					//$entry_id = metodo_per_ricavare_id($entry);
+					//$entry = createEntry($xml_pathway_obj['name'],$entry_id,$xml_item['name'],$xml_item['type'],$xml_item['link'],$graph_child);
+		            //$entryRepo->add($entry);
+				}
+				
+				
 			}
-			else if ($xmlobj_child -> getName() == "relation")
+			else if ($xml_item -> getName() == "relation")
 			{
-				$subtype_entry = $xmlobj_child -> children(); // figlio "graphic"
-				// leggo i campi di relation dall'xml
-				$entry1 = $allEntries[$xmlobj_child['entry1']];
-	            $entry2 = $allEntries[$xmlobj_child['entry2']];
-	            //costruisco la relazione tra le due entry lette
-	            $relation = createRelation($entry1, $entry2, $relationTypeRepo->get($xmlobj_child['type']),
-	                $relationSubTypeRepo->get($subtype_entry['name']), $path);
-	            //aggiungo la relazione alla repository
-	            $relationRepo->add($relation);
-				//costruisco un sottotipo di relazione di esempio
-				// if not exists (subtype_name, subtype_value)
-				// {
-				$compoundRelationSubType = new \Mithril\Pathway\Relation\SubType(['name' => $subtype_entry['name'], 'value' => $subtype_entry['value']]);
-				//e lo aggiungo alla relativa repository
-				$relationSubTypeRepo->add($compoundRelationSubType);
-				// }
+				foreach ($xml_item->children() as $subtype_entry)
+				{
+					// leggo i campi di relation dall'xml
+					$entry1 = $allEntries[$xml_item['entry1']];
+		            $entry2 = $allEntries[$xml_item['entry2']];
+					//costruisco la relazione tra le due entry lette
+		            $relation = createRelation($entry1, $entry2, $relationTypeRepo->get($xml_item['type']),
+		                $relationSubTypeRepo->get($subtype_entry['name']), $path);
+		            $relationRepo->add($relation);
+					//costruisco un sottotipo di relazione di esempio
+					$compoundRelationSubType = new \Mithril\Pathway\Relation\SubType(['name' => $subtype_entry['name'], 'value' => $subtype_entry['value']]);
+					$relationSubTypeRepo->add($compoundRelationSubType);
+				}
 			}
-			$allEntries = $path->getEntries();
-		}
-
-	}
+			//$allEntries = $path->getEntries();
+		} 
+	} 
 }
 
 function createPathway($id, $org, $title, $image, $link)
@@ -141,11 +159,13 @@ function createPathway($id, $org, $title, $image, $link)
 	    ]);
 	}
 
-function createEntry($pathway, $id, $name, $type, $link, $graph_entry)
+
+// $id va ricavato incrociando la ricerca; $name è la lista degli alias del'attributo "name" del tag "entry"
+function createEntry($pathway, $id, $name, $type, $link, $graph_child)
 	{
-		return new \Mithril\Pathway\Entry\Entry([ // crea una nuova entry nell'apposito repo
+		return new \Mithril\Pathway\Entry\Entry([
 		        'id'        => $id,
-		        'aliases'   => [],
+		        'aliases'   => $name,
 		        'name'      => $type . $id,
 		        'type'      => $type,
 		        'links'     => $link,
@@ -154,14 +174,14 @@ function createEntry($pathway, $id, $name, $type, $link, $graph_entry)
 		                'pathway' => $pathway,
 		                'graphic' => new \Mithril\Pathway\Graphic([
 		                    'name'    => $type . $id,
-		                    'x'       => $graph_entry -> x,
-		                    'y'       => $graph_entry -> y,
-		                    'coords'  => $graph_entry -> x.','.$graph_entry -> y, // coords = x,y
-		                    'type'    => $graph_entry -> type,
-		                    'width'   => $graph_entry -> width,
-		                    'height'  => $graph_entry -> height,
-		                    'fgcolor' => $graph_entry -> fgcolor,
-		                    'bgcolor' => $graph_entry -> bgcolor
+		                    'x'       => $graph_child -> x,
+		                    'y'       => $graph_child -> y,
+		                    'coords'  => $graph_child -> x.','.$graph_child -> y, // coords = x,y
+		                    'type'    => $graph_child -> type,
+		                    'width'   => $graph_child -> width,
+		                    'height'  => $graph_child -> height,
+		                    'fgcolor' => $graph_child -> fgcolor,
+		                    'bgcolor' => $graph_child -> bgcolor
 		                ])
 		            ])
 		        ]
